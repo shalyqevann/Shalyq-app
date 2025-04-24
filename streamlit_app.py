d0e89e72565e3b645d181f1200189b0c6e399417
shalyqevann <evannmaliq@gmail.com>
2025-04-24 11:02:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Latihan membuat Website")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Latihan membuat website melalui Github dan Streamlit dengan menggunakan bahasa phyton."
 )
