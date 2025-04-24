a3e87c381bde72337461cb1c2457c40df97afc90
shalyqevann <evannmaliq@gmail.com>
2025-04-24 11:04:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 Latihan membuat Website")
 st.write(
-    "Latihan membuat website melalui Github dan Streamlit dengan menggunakan bahasa phyton."
+    "Latihan membuatt website melalui Github dan Streamlit dengan menggunakan bahasa phyton."
 )
